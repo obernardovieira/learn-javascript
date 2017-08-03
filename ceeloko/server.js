27611aa11d5970a1424c75f7b8793906110fa401
Bernardo Vieira <bernardo5@live.com.pt>
2017-08-03 01:19:05 +0100

    mongodb connection

--- ceeloko/server.js
+++ ceeloko/server.js
@@ -1,9 +1,24 @@
+require('dotenv').config()
+
+const MongoClient = require('mongodb').MongoClient
+const mongo_url = 'mongodb://' +
+    process.env.MONGO_USERNAME + ':' +
+    process.env.MONGO_PASSWORD + '@localhost:27017/' +
+    process.env.MONGO_DATABASE
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+var server;
+
+//const fs = require('fs')
+const Log = require('log')
+const log = new Log('info'/*, fs.createWriteStream('my.log')*/)
+
 
 var users = require('./routes/users')
 var loko = require('./routes/loko')
+
 app.use('/user', users)
 app.use('/loko', loko.method)
 app.use(function(request, response, next) {
@@ -11,6 +26,18 @@ app.use(function(request, response, next) {
     response.json(JSON.stringify(myData))
 })
 
-app.listen(port, function () {
-    console.log('Example app listening on port ' + port + '!')
+
+MongoClient.connect(mongo_url, function(error, database) {
+    if(error == null && database != null) {
+        log.info('Login with success!')
+    }
+    else {
+        log.error('Error connecting to database!')
+        server.close()
+    }
+})
+
+
+server = app.listen(port, function () {
+    log.info('Example app listening on port ' + port + '!')
 })
